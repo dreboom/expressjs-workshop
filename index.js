b2bdcf0dac4c85c51f233557afc982a949fbef5e
dreboom <drestorelli@gmail.com>
2016-02-15 18:43:37 +0000

    exercise 1

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+// app.get('/', function (req, res) {
+//   res.send('Hello World!');
+// });
+
+
+
+//Exercise 1 - Create a web server that can listen to requests for /hello, and respond with some HTML that says <h1>Hello World!</h1>
+
+app.get('/hello', function(request, response) {
+  response.send('Hello World!');
 });
+
+
 
 
 
