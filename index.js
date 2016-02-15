38f268ebb65402f952acdb61acd9e5d83d7f0b4c
dreboom <drestorelli@gmail.com>
2016-02-15 20:40:44 +0000

    exercises 1-3

--- index.js
+++ index.js
@@ -24,6 +24,45 @@ app.get('/hello/:firstName', function(request, response) {
 
 
 
+//Exercise 3 - Create a web server that can listen to requests for /op/:operation/:number1/:number2 and respond with a JSON object
+//Your program should work for add,sub,mult,div and return the appropriate solution using response.json 
+//(this function will automatically JSON.stringify anything you give it). 
+//If operation is something other than these 4 values, you should use res.sendStatus to send an appropriate error code.
+
+
+app.get('/op/:operation/:number1/:number2', function(request, response) {
+  
+  var num1 = Number(request.params.number1);
+  var num2 = Number(request.params.number2);
+  var answer;
+  switch (request.params.operation) {
+    case 'add':
+      answer = (num1 + num2);
+      break;
+    case 'sub':
+      answer = (num1 - num2);
+      break;
+    case 'mult':
+      answer = (num1 * num2);
+      break;
+    case 'div':
+      answer = (num1 / num2);
+      break;
+  }
+  var fullObj = {
+    operation: request.params.operation,
+    firstOperand: request.params.number1,
+    secondOperand: request.params.number2,
+    solution: answer
+  };
+
+  answer ? response.json(fullObj) : response.status(400).send("You messed up!");
+
+});
+
+
+
+
 
 
 
